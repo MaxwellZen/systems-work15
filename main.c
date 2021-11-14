a2c2518471e3186f5268f1d0d6c0fe5a9029abcd
Maxwell Zen <maxwell.zen@gmail.com>
2021-11-13 21:57:37 -0500

    used strsep and standard input

--- main.c
+++ main.c
@@ -4,25 +4,21 @@
 #include<unistd.h>
 
 char ** parse_args( char * line ) {
-	int cnt = 1;
-	char *copy = line;
-	while (*copy) {
-		if (*copy==' ') *copy='\0', cnt++;
-		copy++;
-	}
-	printf("%d\n", cnt);
-	char **ans = malloc(cnt * sizeof(char*)+1);
-	ans[cnt]=0;
-	copy = line;
-	for (int i = 0; i < cnt; i++) {
-		ans[i] = copy;
-		copy += strlen(copy)+1;
+	char **ans = malloc(6*sizeof(char*));
+	ans[0] = 0;
+	int index = 0;
+	while (line) {
+		ans[index] = strsep(&line, " ");
+		ans[++index] = 0;
 	}
 	return ans;
 }
 
 int main() {
-	char c[100] = "ls -a -l";
+	char c[500];
+	printf("Enter a command: ");
+	fgets(c, 500, stdin);
+	*strchr(c, '\n') = 0;
 	char **args = parse_args(c);
 	execvp(args[0], args);
 }
